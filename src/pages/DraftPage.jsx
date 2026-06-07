bb7482fb58b6b57adf110597d808d87505ace28b
Даниил Калашников <ddkalashnikov@edu.hse.ru>
2026-06-07 09:32:13 +0300

    front ept ( gpt refactor.... idk )

--- src/pages/DraftPage.jsx
+++ src/pages/DraftPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import useMatchSession from './useMatchSession.js';
+import { useMatchSession } from './useMatchSession.js';
 import './DraftPage.css';
 
 const DraftPage = () => {
